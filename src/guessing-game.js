0f2ef1dcac6f551e498f5e6e4e2700c3cec1bf5b
Grif-Rift-BY <pavel.zalazaev@protonmail.com>
2022-08-16 22:27:35 +0300

    task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
 
-    setRange(min, max) {
+    constructor( min, max ) {
+        this.min = min;
+        this.max = max;
+    }
 
+
+    setRange( min, max ) {
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.middle = Math.round( ( this.max - this.min ) / 2 + this.min );
+        return this.middle;
     }
 
     lower() {
-
+        this.max = this.middle;
     }
 
     greater() {
-
+        this.min = this.middle;
     }
 }
 
